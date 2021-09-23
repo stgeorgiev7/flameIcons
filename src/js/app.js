558f47ab3917ba1bffb0d1aa5a3fa51620c57d9c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-23 16:52:49 +0300

    added in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  const flameIcon = '&#x1F525;'
+  document.querySelectorAll('.hot').forEach(element => {
+    element.innerHTML += flameIcon;
+  });
 });
